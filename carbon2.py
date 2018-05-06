17e6e3b2a7ff079751a221368b56a4c8de10dc56
Jihoon Kim <imsesaok@tuta.io>
2018-05-06 20:13:45 +0800

    Add Multiple Identifier Support for Each Adapters Command Has More Flexibility in Matching Commands Add Metadata Variables Fix Formatting Edit Help Command Behaviour Improve Echo Rule Mechanism; Add deleteif Command

--- carbon2.py
+++ carbon2.py
@@ -3,14 +3,18 @@ import carbon2_commands
 from datetime import datetime
 
 class Adapter(threading.Thread):
+    def __init__(self, identifier="!"):
+        self.identifier=identifier
+
     def register_callback(self, func, _id):
         pass
-    
+
     def send_to(self, message, to):
         pass
 
 class IRCAdapter(Adapter):
     def __init__(self, server, port, is_ssl, channels, owner, nick="Carbon", codec="UTF-8", is_sasl=False, password=None):
+        super(self.__class__, self).__init__()
         self.server = server
         self.port = port
         self.is_ssl = is_ssl
@@ -20,23 +24,23 @@ class IRCAdapter(Adapter):
         self.owner = owner
         self.codec = codec
         self.password = password
-        
+
         threading.Thread.__init__(self)
         self.logger = logging.getLogger("IRCAdapter")
         logging.basicConfig(level=logging.DEBUG)
-    
+
     def ping(self, msg):
         pong = msg.strip("PING :")
         self.raw_send("PONG :%s\n" %pong)
-    
+
     def register_callback(self, func, _id):
         self.execute = func
         self._id = _id
-    
+
     def raw_send(self, message):
         self.sock.send(message.encode(self.codec))
         self.logger.info("Sent: %s" %message)
-        
+
     def send(self, message, to):
         send_to = to.strip()
         if not message.strip("\r\n").strip():
@@ -44,10 +48,10 @@ class IRCAdapter(Adapter):
         for line in message.split("\n"):
             message = line.strip(" ")
             self.raw_send("PRIVMSG " + to + " :" + message + "\n")
-            
+
     def join_channel(self, ch):
         self.raw_send("JOIN %s\n"%ch)
-    
+
     def run(self):
         raw_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         raw_socket.connect((self.server, self.port))
@@ -56,29 +60,28 @@ class IRCAdapter(Adapter):
             self.sock = ssl.wrap_socket(raw_socket)
         else:
             self.sock = raw_socket
-        
+
         if self.is_sasl:
             self.raw_send("CAP REQ :sasl\n")
-        
+
         self.raw_send("NICK %s\n" % self.nick)
         self.raw_send("USER {0} {0} {0} :Carbon, IRC bot made by imsesaok\n".format(self.nick))
-        
 
         self.logger.info("logging in...")
 
         while True:
-            msg = self.sock.recv(512).decode(self.codec).strip()
+            msg = self.sock.recv(512).decode(self.codec).strip().replace(self.nick, "")
             if msg is not "":
                 self.logger.info(msg)
             if "MODE %s"%self.nick in msg or "MOTD" in msg:
                 break
-            
+
             elif "PING :" in msg:
                 self.ping(msg)
-            
+
             elif "CAP" in msg and "ACK" in msg:
                     self.raw_send("AUTHENTICATE PLAIN\n")
-            
+
             elif "AUTHENTICATE" in msg:
                 auth = ('{sasl_username}\0'
                 '{sasl_username}\0'
@@ -87,15 +90,15 @@ class IRCAdapter(Adapter):
                 auth = base64.encodestring(auth.encode(self.codec))
                 auth = auth.decode(self.codec).rstrip('\n')
                 self.raw_send("AUTHENTICATE " + auth +"\n")
-            
+
             elif "903" in msg:
                 self.raw_send("CAP END\n")
-            
+
         for ch in self.channels:
             self.join_channel(ch)
-        
+
         self.logger.info("Logged in succesfully!")
-        
+
         while True:
             try:
                 msg = self.sock.recv(2048).decode(self.codec).strip()
@@ -107,68 +110,72 @@ class IRCAdapter(Adapter):
                     ch = msg.split("PRIVMSG")[-1].split(" :")[0]
                     user = msg.split("!")[0].split(":", 1)[1]
                     message = msg.split(":", 2)[2]
-                    metadata = {"from_user": user, "from_group": ch, "when": datetime.now(), "_id": self._id, }
+                    metadata = {"from_user": user, "from_group": ch, "when": datetime.now(),
+                            "_id": self._id, "ident": self.identifier, "type": self.__class__.__name__,
+                            "mentioned": self.nick in message, }
                     self.execute(message, metadata)
-        
+
             except Exception as e:
                 self.logger.error("Error while reading socket.", exc_info=True)
 
 class TelegramAdapter(Adapter):
     def __init__(self, token, admin_id):
+        super(self.__class__, self).__init__(r"[!/]")
         from telegram.ext import Updater
         self.updater = Updater(token = token)
         self.dispatcher = self.updater.dispatcher
         self.bot = self.updater.bot
+        self.bot_id = self.bot.get_me()["username"]
         self.admin_id = admin_id
         threading.Thread.__init__(self)
-    
+
     def run(self):
         from telegram.ext import MessageHandler, CommandHandler, Filters
-        
-        handler = MessageHandler(Filters.text, self.eval)
-        start_handler = CommandHandler('start', self.eval)
-        help_handler = CommandHandler('help', self.eval)
-        
+        handler = MessageHandler(Filters.text | Filters.command, self.eval)
         self.dispatcher.add_handler(handler)
-        self.dispatcher.add_handler(start_handler)
-        self.dispatcher.add_handler(help_handler)
         self.updater.start_polling()
-    
+
     def register_callback(self, func, _id):
         self.callback = func
         self._id = _id
-    
+
     def eval(self, bot, update):
-        message = update.message.text.strip()
+        message = update.message.text.strip().replace("@"+self.bot_id, "")
         from_user = update.message.from_user.name
         from_group = update.message.chat_id
         when = update.message.date
-        metadata = {"from_user": from_user, "from_group": from_group, "when": when, "_id": self._id, }
+        metadata = {"from_user": from_user, "from_group": from_group, "when": when,
+                "_id": self._id, "ident": self.identifier, "type": self.__class__.__name__,
+                "mentioned": "@"+self.bot_id in message }
         self.callback(message, metadata)
-    
+
     def send(self, message, to):
         self.bot.send_message(chat_id=to, text=message)
 
 class Carbon:
-    def __init__(self, adapters = {"adapter":Adapter()}, commands = []):
+    def __init__(self, adapters, commands = []):
         self.commands = commands
         self.adapters= adapters
         for _id in self.adapters:
             self.adapters[_id].register_callback(self.process, _id)
-    
+
     def run(self):
         for adapter in self.adapters.values():
             adapter.start()
-    
+
     def process(self, message, metadata):
         for command in self.commands:
-            match = re.search("^%s$"%command.regex, message)
+            if command.raw_match:
+                match = message if command.regex == message else None
+            else:
+                regex = command.regex.format(ident=metadata["ident"])
+                match = re.search("^{}$".format(regex), message)
             if match is not None:
                 command.on_exec(match, metadata, self)
-    
+
     def send(self, message, to, _id):
         self.adapters[_id].send(message, to)
-    
+
     def finalise(self):
         for adapter in self.adapters.values():
             adapter.finalise()
--- carbon2_commands.py
+++ carbon2_commands.py
@@ -1,37 +1,43 @@
 import random, re, sys, time, traceback
 
 class Command:
-    def __init__(self, regex, title, description, on_exec = None, canned = "", criteria = lambda message, metadata, bot: True):
+    def __init__(self, regex, title, description, on_exec, raw_match=False,
+            display_condition = lambda message, metadata, bot: True, flags = list()):
         self.regex = regex
         self.title = title
         self.description = description
-        if on_exec is not None:
-            self.on_exec = self.run_on_sandbox
-            self.func = on_exec
-        else:
-            self.on_exec = self.canned
-        self.canned_response = canned
-        self.criteria = criteria
-    
-    def canned(self, message, metadata, bot):
-        bot.send(self.canned_response, metadata['from_group'], metadata['_id'])
-    
+        self.on_exec = self.run_on_sandbox
+        self.func = on_exec
+        self.display_condition = display_condition
+        self.flags = flags
+        self.raw_match = raw_match
+
     def run_on_sandbox(self, match, metadata, bot):
         try:
             self.func(match, metadata, bot)
         except:
             traceback.print_exc()
             e = sys.exc_info()[0]
-            bot.send("Oops! Something Went Horribly Wrong! (%s)" % e, metadata['from_group'], metadata['_id'])
+            bot.send("Oops! Something Went Horribly Wrong! (%s)" % e,
+                    metadata['from_group'], metadata['_id'])
+
+class CannedResponseCommand(Command):
+    def __init__(self, regex, title, description, canned = "", raw_match=False,
+            display_condition = lambda message, metadata, bot: True):
+        super(self.__class__, self).__init__(regex, title, description, self.canned, raw_match, display_condition)
+        self.canned_response = canned
+
+    def canned(self, message, metadata, bot):
+        bot.send(self.canned_response, metadata['from_group'], metadata['_id'])
 
 def print_help(match, metadata, bot):
     linecount = 4
     string = ""
     command_list = list()
     for command in bot.commands:
-        if command.criteria(match[0], metadata, bot):
+        if command.display_condition(match[0], metadata, bot):
             command_list.append(command)
-    
+
     command_count = len(command_list)
     try:
         index = int(match['page'].strip()) - 1
@@ -39,23 +45,25 @@ def print_help(match, metadata, bot):
         bot.send("Invalid argument: expecting number.", metadata['from_group'], metadata['_id'])
     except AttributeError:
         index = 0
-    
-    
+
     start = index * linecount
     end = start + linecount
-    
+
     maximum = int(-(-command_count // linecount))
     if command_count < end:
         end = command_count
         if end <= start:
-            bot.send("Invalid number: use number below " + str(maximum) + ".", metadata['from_group'], metadata['_id'])
+            bot.send("Invalid number: use number below " + str(maximum) + ".",
+                metadata['from_group'], metadata['_id'])
             return
-    
+
+    bot.send("Usage: <identifier><command>\nIdentifier setting for the current adapter: {}\n".format(metadata["ident"]), metadata['from_group'], metadata['_id'])
+
     message = "Commands: " + str(index + 1) + " out of " + str(maximum)
     for i in range(start, end):
         command = bot.commands[i]
         message += "\n"+ command.title + ": " + command.description
-    
+
     bot.send(message, metadata['from_group'], metadata['_id'])
 
 def dice(match, metadata, bot):
@@ -67,7 +75,7 @@ def dice(match, metadata, bot):
         return
     except AttributeError:
         pass
-    
+
     if number < 1:
         bot.send("Hey, thought I can't do math? Your number is too small!", metadata['from_group'], metadata['_id'])
     elif number is 1:
@@ -75,38 +83,57 @@ def dice(match, metadata, bot):
     else:
         bot.send(str(random.randrange(1, number+1)) + "!", metadata['from_group'], metadata['_id'])
 
+def echo_exec(message, metadata_lambda, metadata, bot, reply):
+    if metadata_lambda['from_group'] == metadata['from_group'] and metadata_lambda["_id"] == metadata["_id"]:
+        bot.send(reply, metadata['from_group'], metadata['_id'])
+
 def add_echo_command(match, metadata, bot):
-    condition = match['condition']
-    reply = match['reply']
-    echo_reply = Command(condition, "!if %s"%condition, "!echo %s"%reply,
-        lambda message, metadata_lambda, bot: [bot.send(reply, metadata['from_group'], metadata['_id'])
-            if metadata_lambda['from_group'] == metadata['from_group'] else None],
-        criteria = lambda message, metadata, bot: False)
+    condition = match['condition'].strip()
+    reply = match['reply'].strip()
+    echo_reply = Command(condition, "if %s"%condition, "echo %s"%reply,
+        lambda message, metadata_lambda, bot: echo_exec(message, metadata_lambda, metadata, bot, reply),
+        raw_match=True, display_condition = lambda message, metadata, bot: False, flags=["echo"])
+
     for command in bot.commands:
-        if command.title == "!if " + condition:
+        if condition == command.regex and "echo" in command.flags:
             bot.commands.remove(command)
             bot.commands.append(echo_reply)
             bot.send("Rule Already Exists. Overridden.", metadata['from_group'], metadata['_id'])
             return
-    
+        elif re.search("^{ident}{command}$".format(ident=metadata["ident"], command=command.regex), condition) and not command.raw_match:
+            bot.send("Rule Not Accepted: You cannot override existing commands.", metadata['from_group'], metadata['_id'])
+            return
+
     bot.commands.append(echo_reply)
-    bot.send("Rule Created.", metadata['from_group'], metadata['_id'])
+    bot.send("Rule successfully created.", metadata['from_group'], metadata['_id'])
+
+def remove_echo_command(match, metadata, bot):
+    condition = match['condition']
+
+    for command in bot.commands:
+        if condition == command.regex and "echo" in command.flags:
+            bot.commands.remove(command)
+            bot.send("Rule successfully deleted.", metadata['from_group'], metadata['_id'])
+            return
+
+    bot.send("Rule does not exist.", metadata['from_group'], metadata['_id'])
 
 from datetime import datetime, date, timedelta
-about = Command(r"!about", "!about", "Show information about this bot.",
+about = CannedResponseCommand(r"about", "about", "Show information about this bot.",
     canned = "Carbon 2.0 alpha\nA Multi-Protocol Bot developed by imsesaok.\nhttps://github.com/qtwyeuritoiy/CarbonBot2")
-ping = Command(r"!ping", "!ping", "Test the connection between the user and the bot.", canned = "Pong!")
+ping = CannedResponseCommand(r"{ident}ping", "ping", "Test the connection between the user and the bot.", canned = "Pong!")
 
-dice = Command(r"!dice(?: (?P<number>\d+))?", "!dice <number>", "Roll a dice.", dice)
-help = Command(r"(?:/|!)help(?: ?(?P<page>\d+))?|/start", "!help <page>", "Show this text.", print_help)
-echo = Command(r"!echo (?P<message>.+)", "echo <message>", "Echo message.",
+dice = Command(r"{ident}dice(?: (?P<number>\d+))?", "dice <number>", "Roll a dice.", dice)
+help = Command(r"{ident}help(?: ?(?P<page>\d+))?", "help <page>", "Show this text.", print_help)
+echo = Command(r"{ident}echo (?P<message>.+)", "echo <message>", "Echo message.",
     lambda match, metadata, bot: bot.send(match['message'], metadata['from_group'], metadata['_id']))
-add_echo = Command(r"!if (?P<condition>.+?) !?echo (?P<reply>.+)", "!if <message> (!)echo <message>",
+add_echo = Command(r"(?P<ident>{ident})if (?P<condition>.+?) (?P=ident)?echo (?P<reply>.+)", "if <message> echo <message>",
     "Makes the bot reply to certain messages.", add_echo_command)
+remove_echo = Command(r"{ident}removeif (?P<condition>.+?)", "removeif <message>",
+    "Remove the echo rule.", remove_echo_command)
 
-
-uptime = Command(r"!uptime", "!uptime", "Show how long the bot has been operating.",
+uptime = Command(r"{ident}uptime", "uptime", "Show how long the bot has been operating.",
     lambda match, metadata, bot: bot.send(str(timedelta(seconds=time.time() - start_time)), metadata['from_group'], metadata['_id']))
 start_time = time.time()
 
-commands = [help, about, dice, ping, uptime, echo, add_echo, ]
+commands = [help, about, ping, dice, uptime, echo, add_echo, remove_echo, ]
