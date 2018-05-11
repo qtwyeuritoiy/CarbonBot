8e41f7268b98fef9f0cb750f7096bb5d71431f22
M1dgard <M1dgard@users.noreply.github.com>
2018-05-11 22:48:23 +0200

    Add hangman game

--- carbon2_commands.py
+++ carbon2_commands.py
@@ -1,38 +1,6 @@
 import random, re, sys, time, traceback
-
-class Command:
-    def __init__(self, regex, title, description, on_exec, raw_match=False,
-            display_condition = lambda match, metadata, bot: True,
-            exec_condition = lambda match, metadata, bot: True, flags = list()):
-        self.regex = regex
-        self.title = title
-        self.description = description
-        self.on_exec = self.run_on_sandbox
-        self.func = on_exec
-        self.display_condition = display_condition
-        self.exec_condition = exec_condition
-        self.flags = flags
-        self.raw_match = raw_match
-
-    def run_on_sandbox(self, match, metadata, bot):
-        try:
-            if self.exec_condition(match, metadata, bot):
-                self.func(match, metadata, bot)
-        except:
-            traceback.print_exc()
-            e = sys.exc_info()[0]
-            bot.send("Oops! Something Went Horribly Wrong! (%s)" % e,
-                    metadata['from_group'], metadata['_id'])
-
-class CannedResponseCommand(Command):
-    def __init__(self, regex, title, description, canned = "", raw_match=False,
-            display_condition = lambda match, metadata, bot: True,
-            exec_condition = lambda match, metadata, bot: True, flags = list()):
-        super(self.__class__, self).__init__(regex, title, description, self.canned, raw_match, display_condition, exec_condition, flags)
-        self.canned_response = canned
-
-    def canned(self, match, metadata, bot):
-        bot.send(self.canned_response, metadata['from_group'], metadata['_id'])
+from carbon2_command_classes import CannedResponseCommand, Command
+import hangman
 
 about = CannedResponseCommand(r"{ident}about", "about", "Show information about this bot.",
     canned = "Carbon 2.0 alpha\nA Multi-Protocol Bot developed by imsesaok.\nhttps://github.com/qtwyeuritoiy/CarbonBot2")
@@ -191,4 +159,4 @@ regex_switch = Command(r"#regexif(?: (?P<bool>true|false))?", "", "", switch_reg
     display_condition = lambda message, metadata, bot: False,
     exec_condition = lambda message, metadata, bot: metadata["is_mod"], flags=["mod"])
 
-commands = [help, about, ping, dice, uptime, echo, add_echo, remove_echo, list_echo, regex_switch, ]
+commands = [help, about, ping, dice, uptime, echo, add_echo, remove_echo, list_echo, regex_switch, *hangman.commands, ]
---
+++ hangman.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python3
+
+from random import choice, randint
+from carbon2_command_classes import Command
+
+# Word selection stuff
+
+with open('./hangman_words.txt', 'r') as _word_list_file:
+    _WORD_LIST = set(map(
+        lambda x: x.rstrip('\n').lower(),
+        _word_list_file.readlines()
+    ))
+
+
+def candidate(word, pattern, previous_letters):
+    return (
+            # Length must be the same
+            len(word) == len(pattern) and
+
+            # All letters must either
+            #  1) match and have been guessed or
+            #  2) be blank and have not been guessed
+            all(
+                (letter == pattern_char and letter in previous_letters) or (pattern_char == '_' and letter not in previous_letters)
+                for letter, pattern_char in zip(word, pattern)
+               )
+           )
+
+
+def fill(word, pattern, letter_to_reveal):
+    return ''.join(
+                   letter if letter == letter_to_reveal else pattern_char
+                   for letter, pattern_char in zip(word, pattern)
+                  )
+
+
+def select(pattern, previous_letters, guessed_letter):
+    possible_patterns = {}
+    best_option, best_option_possibilities = None, None
+
+    for word in _WORD_LIST:
+        if word and candidate(word, pattern, previous_letters):
+            new_pattern = fill(word, pattern, guessed_letter)
+
+            possibilities = possible_patterns.get(new_pattern)
+            if possibilities is None:
+                possible_patterns[new_pattern] = possibilities = []
+
+            possibilities.append(word)
+
+            if best_option_possibilities is None or len(possibilities) > len(best_option_possibilities):
+                best_option, best_option_possibilities = new_pattern, possibilities
+
+    # Return None when there are no fitting words in our word list
+    if best_option is None:
+        return None
+
+    if len(best_option_possibilities) == 1 and '_' not in best_option:
+        # If the best we can do is with only one word, try to find one that is not the solution, so that
+        # >>> select('hell_', 'halgktes', 'o')
+        # won't select "hello", it can choose "hell_" for "helly"
+        better_option, better_option_possibilities = None, None
+        for option, possibilities in possible_patterns.items():
+            if '_' in option:
+                better_option, better_option_possibilities = option, possibilities
+                break
+
+        # Select a valid pattern that's not the solution, but only if there is such a pattern
+        if better_option is not None:
+            best_option, best_option_possibilities = better_option, better_option_possibilities
+
+    return (best_option, choice(best_option_possibilities))
+
+
+# Chat driver stuff
+
+TOLERANCE = 7
+
+chat_games = {}
+
+def chat_get_game(match, metadata, bot):
+    adapter_id = metadata["_id"]
+    channel = metadata["from_group"]
+
+    if adapter_id not in chat_games:
+        chat_games[adapter_id] = {}
+        return None
+
+    if channel not in chat_games[adapter_id]:
+        return None
+
+    return chat_games[adapter_id][channel]
+
+
+def chat_new_game(match, metadata, bot):
+    adapter_id = metadata["_id"]
+    channel = metadata["from_group"]
+
+    if adapter_id not in chat_games:
+        chat_games[adapter_id] = {}
+
+    word_len = randint(5, 15)
+    game = {
+        "word_len": word_len,
+        "pattern": '_' * word_len,
+        "previous_letters": set(),
+        "misses_left": TOLERANCE
+    }
+    chat_games[adapter_id][channel] = game
+    return game
+
+
+def chat_end(match, metadata, bot):
+    adapter_id = metadata["_id"]
+    channel = metadata["from_group"]
+
+    if adapter_id not in chat_games:
+        chat_games[adapter_id] = {}
+
+    if channel in chat_games[adapter_id]:
+        del chat_games[metadata['_id']]
+
+
+def chat_start(match, metadata, bot):
+    game = chat_get_game(match, metadata, bot)
+
+    if game:
+        bot.send("Send a message consisting of one letter to make a guess.", metadata['from_group'], metadata['_id'])
+    else:
+
+        game = chat_new_game(match, metadata, bot)
+
+        bot.send("Started a new hangman game in English! The word is {word_len} letters long. Guess a letter…".format(word_len=game["word_len"]),
+                 metadata['from_group'], metadata['_id'])
+
+    chat_status(match, metadata, bot)
+
+
+def chat_guess(match, metadata, bot):
+    game = chat_get_game(match, metadata, bot)
+
+    if not game:
+        if match['cmd']:
+            bot.send("There's no hangman game going on here! Start one first by doing !hangman", metadata['from_group'], metadata['_id'])
+        return
+
+    guessed_letter = match["letter"].lower()
+    if guessed_letter == "_":
+        bot.send("No words contain underscores. Lol, that'd be confusing.", metadata['from_group'], metadata['_id'])
+        chat_status(match, metadata, bot)
+        return
+
+    game["pattern"], matching_word = select(game["pattern"], game["previous_letters"], guessed_letter)
+    game["previous_letters"].add(guessed_letter)
+    if guessed_letter not in game["pattern"]:
+        game["misses_left"] -= 1
+
+    if '_' not in game["pattern"]:
+        # Guessed the word!
+        chat_status(match, metadata, bot)
+        chat_end(match, metadata, bot)
+        bot.send("Congratulations, you guessed the word!", metadata['from_group'], metadata['_id'])
+        return
+
+    if not game["misses_left"]:
+        chat_status(match, metadata, bot)
+        chat_end(match, metadata, bot)
+        bot.send("Game over! The word was {}.".format(matching_word.upper()), metadata['from_group'], metadata['_id'])
+        return
+
+    chat_status(match, metadata, bot)
+
+
+def chat_status(match, metadata, bot):
+    game = chat_get_game(match, metadata, bot)
+
+    bot.send('{pattern} {left}/{total}'.format(pattern=" ".join(game["pattern"].upper()),
+                                               left=game["misses_left"],
+                                               total=TOLERANCE
+                                               ),
+             metadata['from_group'], metadata['_id']
+             )
+
+
+commands = (
+    Command(r"{ident}hangman(?: .*)?", "Play a game of hangman", "Find the word by guessing one letter at a time", chat_start),
+    Command(r"(?P<cmd>{ident}guess )?(?P<letter>.)", "", "", chat_guess,
+            display_condition = lambda message, metadata, bot: False,
+            ),
+)
+
